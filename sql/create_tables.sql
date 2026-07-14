544dc94b1c3660a8afa7ede1b94f6b435d5654d9
Esraa Ehab <esraa.ehab105@gmail.com>
2026-07-14 23:49:14 +0300

    Add user and user_watchlist tables with foreign key constraints

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -20,3 +20,29 @@ CREATE TABLE raw.pipeline_metadata (
     last_run_timestamp TIMESTAMP,
     status TEXT
 );
+
+create table if not exists app.users (
+    user_id GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL, 
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS app.user_watchlists (
+    user_id INT NOT NULL,
+    movie_id INT NOT NULL,
+    added_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    
+    PRIMARY KEY (user_id, movie_id),
+    
+    CONSTRAINT fk_watchlist_user 
+        FOREIGN KEY (user_id) 
+        REFERENCES app.users (user_id) 
+        ON DELETE CASCADE,
+        
+    CONSTRAINT fk_watchlist_movie 
+        FOREIGN KEY (movie_id) 
+        REFERENCES app.dim_movies_clean (movie_id) 
+        ON DELETE CASCADE
+);
